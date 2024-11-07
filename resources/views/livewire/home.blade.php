2a159ddbd035a612ef0d75c2296dec3a81a34542
Jorgerm04 <jorgeroch2004@gmail.com>
2024-11-07 08:17:15 +0100

    Update home.blade.php

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -45,7 +45,7 @@
         @endforeach
     </main>
     <div class="flex flex-col gap-y-3 justify-center bg-slate-500 text-white p-4 m-2 rounded-lg text-center">
-        <p>Para que dar de alta un almacen pongase en contacto con el numero +34 657 56 71 29 .</p>
+        <p>Para dar de alta un almacén póngase en contacto con el número +34 657 56 71 29 .</p>
         <div class="flex flex-row text-center gap-x-4 justify-center items-center">
             <a href="https://wa.me/657567129">
                 <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class=""><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 21l1.65 -3.8a9 9 0 1 1 3.4 2.9l-5.05 .9" /><path d="M9 10a.5 .5 0 0 0 1 0v-1a.5 .5 0 0 0 -1 0v1a5 5 0 0 0 5 5h1a.5 .5 0 0 0 0 -1h-1a.5 .5 0 0 0 0 1" /></svg>
